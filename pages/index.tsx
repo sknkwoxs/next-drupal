34abd1c93078de373fd8aea3bf52ed5a90f40647
sknk-dev <sknk-dev@users.noreply.github.com>
2022-07-08 17:26:14 +0900

    field_media_image 관련 오류로 삭제합니다.

--- pages/index.tsx
+++ pages/index.tsx
@@ -46,8 +46,8 @@ export async function getStaticProps(
     {
       params: {
         "filter[status]": 1,
-        "fields[node--article]": "title,path,field_media_image,uid,created",
-        include: "field_media_image.field_media_image,uid",
+        "fields[node--article]": "title,path,uid,created",
+        include: "uid",
         sort: "-created",
       },
     }
