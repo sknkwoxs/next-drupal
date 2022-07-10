e43ba1a18514b32527c27616f9b7d8c41e458ab4
sknk-dev <sknk-dev@users.noreply.github.com>
2022-07-10 15:59:03 +0900

    빌드 오류를 바로잡습니다.

--- pages/api/preview.ts
+++ pages/api/preview.ts
@@ -6,5 +6,5 @@ export default async function handler(
   request: NextApiRequest,
   response: NextApiResponse
 ) {
-  // return await drupal.preview(request, response)
+  return await drupal.preview(request, response)
 }
